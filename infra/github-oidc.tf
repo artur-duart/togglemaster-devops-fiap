ad00156c5223ff29f0236df861f26ffd5c4eb310
Artur Duarte <arturduartemoraes@gmail.com>
2026-08-07 23:38:48 -0300

    feat(infra): add GitHub Actions OIDC provider and ECR push role

---
+++ infra/github-oidc.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:artur-duart/togglemaster-devops-fiap:*"] # 🆕 confirme o dono/nome do repo
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "togglemaster-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume.json
+}
+
+data "aws_iam_policy_document" "ecr_push" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+    resources = [for repo in aws_ecr_repository.this : repo.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "ecr_push" {
+  name   = "ecr-push"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.ecr_push.json
+}
+
+output "github_actions_role_arn" {
+  description = "ARN da role que o GitHub Actions assume via OIDC"
+  value       = aws_iam_role.github_actions.arn
+}
