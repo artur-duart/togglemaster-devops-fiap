1860bbc645ef3cfa61bce1eb2f1881ae6c05b6ea
Artur Duarte <arturduartemoraes@gmail.com>
2026-09-02 00:52:46 -0300

    feat(infra): install ArgoCD via Terraform helm provider

---
+++ infra/argocd.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "10.6.0"
+  namespace        = "argocd"
+  create_namespace = true
+}
